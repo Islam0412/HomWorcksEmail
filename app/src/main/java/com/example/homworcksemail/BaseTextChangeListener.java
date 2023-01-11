e7e5df98701ccd6edb6a547dae30a7fded87d978
islam0412 <islamlivaza2@gmail.com>
2023-01-11 18:23:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homworcksemail/BaseTextChangeListener.java
@@ -0,0 +1,21 @@
+package com.example.homworcksemail;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public class BaseTextChangeListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
